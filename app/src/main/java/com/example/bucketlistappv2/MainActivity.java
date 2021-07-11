d01c1cebf8a853efb41e037ca83eb82fddfcbcdb
apoorv127 <prasada@uwm.edu>
2021-07-11 02:11:38 -0500

    RatingBar, 'style' first look, 'float' decimal variable type

--- app/src/main/java/com/example/bucketlistappv2/MainActivity.java
+++ app/src/main/java/com/example/bucketlistappv2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.bucketlistappv2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
+        CardView thingsToDoCard = findViewById(R.id.card_view_things_to_do);
+        CardView placesToGoCard = findViewById(R.id.card_view_places_to_go);
+
+        thingsToDoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        placesToGoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/bucketlistappv2/ThingsToDoActivity.java
@@ -0,0 +1,14 @@
+package com.example.bucketlistappv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThingsToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_things_to_do);
+    }
+}
